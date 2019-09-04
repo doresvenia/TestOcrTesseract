c109a39f2053882c8d5ba8697ddae6ee11fa3c00
Choosak <choosak_jitt@hotmail.com>
2019-09-04 17:15:14 +0700

    Add project files.

---
+++ TestOcrTesseract/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestOcrTesseract.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:FileUpload ID="fu" runat="server" accept="image/*" />
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        <br />
+        <asp:Label ID="lblResult" runat="server"></asp:Label>
+        <br />
+        <hr />
+        Reference:
+        <a href="http://diegogiacomelli.com.br/using-tesseract4-with-csharp/">
+            Using Tesseract 4 with C#
+        </a>
+    </form>
+</body>
+</html>
---
+++ TestOcrTesseract/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using OCR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestOcrTesseract
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            TesseractService service = new TesseractService(@"C:\Program Files\Tesseract-OCR", "tha", @"C:\Program Files\Tesseract-OCR\tessdata");
+
+            // var stream = File.OpenRead(string path);
+            // var stream = WebRequest.Create(string url).GetResponse().GetResponseStream();
+            // var stream = new MemoryStream(byte[] buffer);
+            //var text = service.GetText(stream);
+
+            lblResult.Text = service.GetText(fu.PostedFile.InputStream);
+        }
+    }
+}
